09be1b492b12c1e96646119681ee11623685b1c8
vox09 <xuxinyuan09@gmail.com>
2019-09-24 10:48:26 +0800

    Verify function setPWMDutyCycle

--- Src/pwm.c
+++ Src/pwm.c
@@ -1,5 +1,6 @@
 #include "tim.h"
 #include "pwm.h"
+#include <stdio.h>
 
 void StartPWMTask(void const * argument)
 {
@@ -7,26 +8,30 @@ void StartPWMTask(void const * argument)
     HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_2);
     HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_3);
     HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_4);
+    setPWMDutyCycle(1, 50);
     for(;;);
 }
 
 void setPWMDutyCycle(uint8_t channel, float percent)
 {
+    percent = percent>100.0?100.0:percent;
+    percent = percent<0.0?0.0:percent;
     switch (channel)
     {
     case 1:
-        TIM5->CCR1 = (uint16_t)percent * 500/100;
+        TIM5->CCR1 = (uint16_t)(percent * 500/100);
         break;
     case 2:
-        TIM5->CCR2 = (uint16_t)percent * 500/100;
+        TIM5->CCR2 = (uint16_t)(percent * 500/100);
         break;
     case 3:
-        TIM5->CCR3 = (uint16_t)percent * 500/100;
+        TIM5->CCR3 = (uint16_t)(percent * 500/100);
         break;
     case 4:
-        TIM5->CCR4 = (uint16_t)percent * 500/100;
+        TIM5->CCR4 = (uint16_t)(percent * 500/100);
         break;
     default:
+        printf("Incorrect PWM channel!");
         break;
     }
 }
