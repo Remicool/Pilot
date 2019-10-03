bbcb0642b9dd9e12c5a503708e2248c9d8da5f61
vox09 <xuxinyuan09@gmail.com>
2019-10-03 19:50:14 +0800

    Config LPTIM Encoder

--- Src/motor.c
+++ Src/motor.c
@@ -3,6 +3,9 @@
 #include "motor.h"
 #include <stdio.h>
 
+// Hall resolution 695
+#define ENCODER_ONE_CIRCLE 695.1f
+
 void StartMotorTask(void const * argument)
 {
     HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
@@ -17,13 +20,13 @@ void StartMotorTask(void const * argument)
     TIM1->CCR2= (uint16_t)0;
     TIM1->CCR3= (uint16_t)500;
     TIM1->CCR4= (uint16_t)0;
-    uint16_t enc1, enc2;
+    float ang1, ang2;
     for(;;)
     {
-        enc1 = (uint16_t)TIM5->CNT;
-        enc2 = (uint16_t)LPTIM1->CNT;
-        printf("encoder value:\t%d\r\n", enc1);
-        printf("LPTIM encoder value:\t%d\r\n", enc2);
+        ang1 = TIM5->CNT  / ENCODER_ONE_CIRCLE;
+        ang2 = LPTIM1->CNT/4  / ENCODER_ONE_CIRCLE;
+        printf("encoder value:      \t%f\r\n", ang1);
+        printf("LPTIM encoder value:\t%f\r\n", ang2);
         osDelay(500);
     };
 }
